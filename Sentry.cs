e86cd41cb51c012b95d838e3243c4164cda48c70
David <Dankerprouduct2@gmail.com>
2015-09-28 18:03:07 -0400

    Revert "Sentry Damage"
    This reverts commit 59843346f2271e757039dde0445222395baf2e29.

--- Sentry.cs
+++ Sentry.cs
@@ -263,7 +263,7 @@ public class Sentry : MonoBehaviour {
 
     public void ClientTakeDamageFromWeapon(int dam)
     {
-        health = health - (dam / 2); 
+        health = health - dam; 
     }
     #region Network Junk
     [RPC]
@@ -338,7 +338,7 @@ public class Sentry : MonoBehaviour {
     [RPC]
     void NetworkTakeDamageFromWeapon(int damage)
     {
-        health = health - (damage / 2);
+        health = health - damage;
         CheckIfDead(); 
     }
     #region NetworkSyncing
