0ed893d3b1e0e65cada289b23d0a8fdfa0355cfd
David <Dankerprouduct2@gmail.com>
2015-09-29 23:51:42 -0400

    Syncs Movement across network for the health shards

--- SentryHealth.cs
+++ SentryHealth.cs
@@ -8,6 +8,19 @@ public class SentryHealth : MonoBehaviour {
     Transform bossPosition;
     Transform startPosition;
     public int startPos;
+    #region
+
+    private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
+
+    private Quaternion roatation = Quaternion.identity;
+    private Quaternion qSrot = Quaternion.identity;
+    private Quaternion qErot = Quaternion.identity;
+
+    #endregion
     // Use this for initialization
     void Start()
     {
@@ -32,6 +45,10 @@ public class SentryHealth : MonoBehaviour {
         {
             MainAi();
         }
+        else
+        {
+            SyncedMovement(); 
+        }
 	}
 
     void MainAi()
@@ -46,4 +63,51 @@ public class SentryHealth : MonoBehaviour {
     {
         health = health - damage; 
     }
+    #region NetworkSyncing
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        
+        Vector3 syncPosition = Vector3.zero;
+        Quaternion rot = Quaternion.identity;
+
+        if (stream.isWriting)
+        {
+
+            syncPosition = transform.position;
+            rot = transform.rotation;
+            
+
+            stream.Serialize(ref syncPosition);
+
+            stream.Serialize(ref rot);
+            
+        }
+        else
+        {
+            stream.Serialize(ref syncPosition);
+            stream.Serialize(ref rot);
+            
+            syncTime = 0f;
+            syncDelay = Time.time - lastSynchronizationTime;
+            lastSynchronizationTime = Time.time;
+
+            syncStartPosition = transform.position;
+            syncEndPosition = syncPosition;
+
+            qSrot = transform.rotation;
+            qErot = rot;
+        }
+
+
+    }
+
+    private void SyncedMovement()
+    {
+        syncTime += Time.deltaTime;
+        transform.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+
+        transform.rotation = Quaternion.Lerp(qSrot, qErot, syncTime / syncDelay);
+
+    }
+    #endregion
 }
